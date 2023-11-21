8ea9013a7451df47c3ffa913a4242505220a704c
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-21 18:40:21 -0300

    🏷️ Nova tabela: Orphanages, no Back-End da Aplicação
    Através do comando: ">_ npx typeorm migration:create -n create_orphanages" criado o Migration Template vazio para a tabela "orphanages", que foi respectivamente desenvolvido de acordo com o escopo da aula, e então com o comando: ">_ npm run typeorm migration:run" foi devidamente aplicada então esta nova Migration no Banco de Dados, no Back-End da Aplicação.

---
+++ backend/src/database/migrations/1700602409398-create_orphanages.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+    export class createOrphanages1700602409398 implements MigrationInterface {
+        public async up(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.createTable(new Table({
+                name: "orphanages",
+                    columns: [
+                        {
+                            name: "id",
+                            type: "integer",
+                            unsigned: true,
+                            isPrimary: true,
+                            isGenerated: true,
+                            generationStrategy: "increment"
+                        },
+                        {
+                            name: "name",
+                            type: "varchar"
+                        },
+                        {
+                            name: "latitude",
+                            type: "decimal",
+                            scale: 10,
+                            precision: 2
+                        },
+                        {
+                            name: "longitude",
+                            type: "decimal",
+                            scale: 10,
+                            precision: 2
+                        },
+                        {
+                            name: "about",
+                            type: "text"
+                        },
+                        {
+                            name: "intructions",
+                            type: "text"
+                        },
+                        {
+                            name: "open-on-weekends",
+                            type: "boolean",
+                            default: false
+                        }
+                    ]
+            }));
+        };
+        public async down(queryRunner: QueryRunner): Promise<void> {
+            await queryRunner.dropTable("orphanages");
+        };
+    };
