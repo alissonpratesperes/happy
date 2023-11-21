169ba9c9f393156cddd440afc937d12e92f2974e
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-21 20:33:11 -0300

    🚩 Novo método em: OrphanagesController, no Back-End da Aplicação
    No controller: "./backend/src/controllers/OrphanagesController" implementado anteriormente, implementado a nova função assíncrona "index" para a listagem dos orfanatos cadastrados no Banco de Dados;
    Seguindo o fluxo de implementação no arquivo das rotas: "./backend/src/routes", implementado o Método "GET" para esta listagem na mesma rota (uma mesma rota pode ser utilizada para os métodos de CRUD) apontando então para o método "index" deste Controller, no Back-End da Aplicação.

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,12 @@ import { Request, Response } from "express";
 import Orphanage from "../models/Orphanage";
 
     export default {
+        async index(request: Request, response: Response) {
+            const orphanagesRepository = getRepository(Orphanage);
+            const orphanages = await orphanagesRepository.find();
+
+                return response.json(orphanages);
+        },
         async create(request: Request, response: Response) {
             const {
                 name,
