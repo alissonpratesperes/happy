a3a6572c4f584f3c7b9245fe9273c9723c5ab9cc
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-21 20:26:59 -0300

    🚩 Novo controller: OrphanagesController, no Back-End da Aplicação
    Implementado o primeiro controller em: "./backend/src/controllers/OrphanagesController" onde abstraiu toda a esquemática de criação de um novo orfanato, que antes estava alocada no arquivo principal da Aplicação, para a função assíncrona "create";
    Criado o novo arquivo de rotas em: "./backend/src/routes", onde serão alocadas as importações e declarações das respectivas rotas e métodos para cada controller implementado, de forma abstraída e desacoplada "dentro do possível" de acordo com o escopo da aula, claro;
    No arquivo: "./backend/src/server" então, apenas reorganizado o código-fonte, importado o arquivo de rotas e configurada a Aplicação para processar este arquivo, ativando todas as rotas que nele estiverem declaradas, assim que inicializar, no Back-End da Aplicação.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,11 @@
 import express from "express";
-import { getRepository } from "typeorm";
 
 import "./database/connection";
-import Orphanage from "./models/Orphanage";
+import routes from "./routes";
 
     const app = express();
 
         app.use(express.json());
+        app.use(routes);
 
-            app.post("/orphanages", async (request, response) => {
-                const {
-                    name,
-                    latitude,
-                    longitude,
-                    about,
-                    instructions,
-                    opening_hours,
-                    open_on_weekends
-                } = request.body;
-                const orphanagesRepository = getRepository(Orphanage);
-                const orphanage = orphanagesRepository.create({
-                    name,
-                    latitude,
-                    longitude,
-                    about,
-                    instructions,
-                    opening_hours,
-                    open_on_weekends
-                });
-
-                    await orphanagesRepository.save(orphanage);
-
-                        return response.status(201).json(orphanage);
-            });
-
-                app.listen(3333);
+            app.listen(3333);
