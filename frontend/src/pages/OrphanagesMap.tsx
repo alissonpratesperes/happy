c5e56123914f4f459414665311e5de52515bdcd2
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-22 20:32:26 -0300

    💄 Nova página: OrphanagesMap, no Front-End da Aplicação
    No diretório: "./frontend/src/pages" na página "OrphanagesMap" implementada a estruturação da mesma de acordo com o escopo do curso abordado em aula, bem como o escopo de estilizações em: "./frontend/src/styles/pages" no arquivo "orphanages-map", seguindo também à risca o mesmo escopo abordado, no Front-End da Aplicação.

--- frontend/src/pages/OrphanagesMap.tsx
+++ frontend/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../assets/images/map-marker.svg";
+import "../styles/pages/orphanages-map.css";
 
     function OrphanagesMap() {
         return (
-            <h1> Hello, world! </h1>
+            <div id="page-map">
+                <aside>
+                    <header>
+                        <img src={ mapMarkerImg } alt="Happy"/>
+                            <h2> Escolha um orfanato no mapa </h2>
+                                <p> Muitas crianças estão esperando a sua visita :) </p>
+                    </header>
+                        <footer>
+                            <strong> São Marcos </strong>
+                                <span> Rio Grande do Sul </span>
+                        </footer>
+                </aside>
+                    <div> </div>
+                        <Link to="" className="create-orphanage">
+                            <FiPlus size={ 32 } color="#FFFFFF"/>
+                        </Link>
+            </div>
         );
     };
 
