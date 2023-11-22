566f750fea4ded4535f07b300c220c5180f2952c
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-22 02:35:37 -0300

    🥅 Validação com o Yup, no Back-End da Aplicação
    No controller: "./backend/src/controllers/OrphanagesController", implementado o "schema" de validação para o cadastro do orfanato com a utilização da dependência "Yup", seguindo o escopo abordado em aula, apenas com um detalhe para a obrigatoriedade do array de imagens que não estava sendo validado, apenas internamente, o que fazia que o orfanato fosse criado sem imagens vinculadas, ou seja, apenas os outros campos eram validados;
    Atualizado o handler de erros: "./backend/src/errors/handler", assim agora retornando corretamente quais campos estão com erros, inclusive quais erros estão sendo emitidos para cada campo que está sendo validado durante a requisição, no Back-End da Aplicação.

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { getRepository } from "typeorm";
 import { Request, Response } from "express";
+import * as Yup from "yup";
 
 import Orphanage from "../models/Orphanage";
 import orphanagesView from "../views/orphanages_view";
@@ -39,7 +40,7 @@ import orphanagesView from "../views/orphanages_view";
                     path: image.filename
                 };
             });
-            const orphanage = orphanagesRepository.create({
+            const data = {
                 name,
                 latitude,
                 longitude,
@@ -48,7 +49,27 @@ import orphanagesView from "../views/orphanages_view";
                 opening_hours,
                 open_on_weekends,
                 images
+            };
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                latitude: Yup.number().required(),
+                longitude: Yup.number().required(),
+                about: Yup.string().required().max(300),
+                instructions: Yup.string().required(),
+                opening_hours: Yup.string().required(),
+                open_on_weekends: Yup.boolean().required(),
+                images: Yup.array(
+                    Yup.object().shape({
+                        path: Yup.string().required()
+                    })
+                ).required()
             });
+
+                await schema.validate(data, {
+                    abortEarly: false
+                });
+
+            const orphanage = orphanagesRepository.create(data);
 
                 await orphanagesRepository.save(orphanage);
 
