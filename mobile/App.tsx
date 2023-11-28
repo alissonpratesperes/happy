5ab515736d9536e100f7120396f4abc33419f425
Alisson Prates Peres <a.pratesperes@gmail.com>
2023-11-28 02:02:08 -0300

    💄 Nova página: OrphanagesMap, no Front-End da Aplicação
    Importados os assets para o MapMarker em: "./mobile/src/images" para a padronização dos marcadores no mapa, assim como feito no Front-End, para marcadores personalizados com a identidade visual desta Aplicação. Com destaque para o novo arquivo: "./mobile/src/@types/index.d.ts" para podermos importar imagens com extensão ".png" como módulos;
    Implementada a nova página: "OrphanagesMap" dentro ainda do arquivo gerado no template padrão: "./mobile/App.tsx" bem como a aplicação de toda a estilização para esta nova página, seguindo o escopo abordado em aula, com algumas melhorias e detalhes na fidelidade de design para com o Front-End, no Mobile da Aplicação.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,10 @@
 import * as Location from "expo-location";
+import { Feather } from "@expo/vector-icons";
 import React, { useState, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Alert } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions, Alert, ActivityIndicator, StatusBar, TouchableOpacity } from "react-native";
+
+import mapMarker from "./src/images/map-marker.png";
 
     export default function App() {
         const [ initialPosition, setInitialPosition ] = useState<[number, number]>([0, 0]);
@@ -23,7 +26,31 @@ import { StyleSheet, Text, View, Dimensions, Alert } from "react-native";
 
                 return (
                     <View style={ styles.container }>
-                        <MapView style={ styles.map } initialRegion={{ latitude: initialPosition[0], longitude: initialPosition[1], latitudeDelta: 0.014, longitudeDelta: 0.014 }} provider={ PROVIDER_GOOGLE }/>
+                        { initialPosition[0] === 0 && initialPosition[1] === 0 ? (
+                            <View style={ styles.loadingMapContainer }>
+                                <ActivityIndicator size="large" color="#000000"/>
+                                    <Text style={ styles.loadingMapText }> Carregando... </Text>
+                            </View>
+                        ) : (
+                            <>
+                                <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+                                    <MapView style={ styles.map } initialRegion={{ latitude: initialPosition[0], longitude: initialPosition[1], latitudeDelta: 0.014, longitudeDelta: 0.014 }} provider={ PROVIDER_GOOGLE }>
+                                        <Marker icon={ mapMarker } coordinate={{ latitude: initialPosition[0], longitude: initialPosition[1] }} calloutAnchor={{ x: 2.8, y: 0.85 }}>
+                                            <Callout tooltip onPress={ () => {} }>
+                                                <View style={ styles.calloutContainer }>
+                                                    <Text style={ styles.calloutText }> São Marquinhos </Text>
+                                                </View>
+                                            </Callout>
+                                        </Marker>
+                                    </MapView>
+                                        <View style={ styles.footerContainer }>
+                                            <Text style={ styles.footerText }> 2 orfanatos encontrados </Text>
+                                                <TouchableOpacity style={ styles.createOrphanageButton } onPress={ () => {} }>
+                                                    <Feather name="plus" size={ 32 } color="#FFFFFF"/>
+                                                </TouchableOpacity>
+                                        </View>
+                            </>
+                        ) } 
                     </View>
                 );
     };
@@ -35,5 +62,52 @@ import { StyleSheet, Text, View, Dimensions, Alert } from "react-native";
             map: {
                 width: Dimensions.get("window").width,
                 height: Dimensions.get("window").height
+            },
+            loadingMapContainer: {
+                width: Dimensions.get("window").width,
+                height: Dimensions.get("window").height,
+                alignItems: "center",
+                justifyContent: "center"
+            },
+            loadingMapText: {
+                marginTop: 16
+            },
+            calloutContainer: {
+                width: 160,
+                height: 46,
+                paddingHorizontal: 16,
+                backgroundColor: "rgba(255, 255, 255, 0.8)",
+                borderRadius: 16,
+                justifyContent: "center"
+            },
+            calloutText: {
+                color: "#0089A5",
+                fontSize: 15
+            },
+            footerContainer: {
+                position: "absolute",
+                left: 30,
+                right: 30,
+                bottom: 30,
+                backgroundColor: "#FFFFFF",
+                borderRadius: 20,
+                height: 60,
+                paddingLeft: 20,
+                paddingRight: 5,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between"
+            },
+            footerText: {
+                color: "#8FA7B3",
+                fontSize: 15
+            },
+            createOrphanageButton: {
+                width: 50,
+                height: 50,
+                backgroundColor: "#15C3D6",
+                borderRadius: 17,
+                alignItems: "center",
+                justifyContent: "center"
             }
         });
